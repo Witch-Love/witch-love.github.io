de7f2c86541d82f01b5adafd012d8e61f6f30834
Singulariity <Singulariity@users.noreply.github.com>
2026-04-16 02:17:57 +0300

    update last update dates

--- mkdocs/docs/js/lastupdatedates.js
+++ mkdocs/docs/js/lastupdatedates.js
@@ -1,4 +1,4 @@
-async function getLastPublishedDate(githubRepo) {
+async function getLastPublishedRelease(githubRepo) {
 	try {
 		const res = await fetch(
 			`https://api.github.com/repos/Witch-Love/${githubRepo}/releases/latest`,
@@ -8,38 +8,81 @@ async function getLastPublishedDate(githubRepo) {
 
 		const data = await res.json();
 
-		return data.published_at ? new Date(data.published_at) : undefined;
+		return {
+			publish: data.published_at
+				? new Date(data.published_at)
+				: undefined,
+			version: data.tag_name
+				? data.tag_name.replace('z', 'r')
+				: undefined,
+		};
 	} catch (error) {
 		console.error('Error fetching Github release data:', error);
 	}
 }
 
-async function replaceLastUpdateDate(githubRepo) {
+async function replaceLastUpdateDate(githubRepo, replaceText) {
 	let dateElement = [...document.querySelectorAll('code')].find(
-		(el) => el.textContent.trim() == '<Tarih>',
+		(el) => el.textContent.trim() == `<${replaceText}>`,
+	);
+	let versionElement = [...document.querySelectorAll('code')].find(
+		(el) => el.textContent.trim() == `<LAST_UPDATE_VERSION>`,
 	);
 
 	if (!dateElement) return;
 
-	const lastPublished = await getLastPublishedDate(githubRepo);
+	dateElement.textContent = 'Tarih bilgisi alınıyor...';
+	if (versionElement)
+		versionElement.textContent = 'Sürüm bilgisi alınıyor...';
 
-	if (!lastPublished) return;
+	const data = await getLastPublishedRelease(githubRepo);
 
-	dateElement.textContent = lastPublished.toLocaleDateString('tr-TR', {
+	if (data.version) {
+		versionElement.textContent = data.version;
+	} else {
+		versionElement.style.color = '#FF0000';
+		dateElement.textContent =
+			'Sürüm bilgisi alınırken hata! Sayfayı yenileyin.';
+	}
+
+	if (!data.publish) {
+		dateElement.style.color = '#FF0000';
+		dateElement.textContent =
+			'Tarih bilgisi alınırken hata! Sayfayı yenileyin.';
+		return;
+	}
+
+	dateElement.textContent = data.publish.toLocaleDateString('tr-TR', {
 		year: 'numeric',
 		month: 'long',
-		weekday: 'long',
+		weekday: 'short',
 		day: 'numeric',
 		hour: 'numeric',
 		minute: 'numeric',
 	});
+
+	if (timeago) {
+		const relativeDate = timeago.format(data.publish, 'tr');
+
+		const timeagoEl = document.createElement('span');
+		timeagoEl.style.color = 'var(--md-code-hl-string-color)';
+		timeagoEl.textContent = ' (' + relativeDate + ')';
+
+		dateElement.insertAdjacentElement('beforeend', timeagoEl);
+	}
 }
 
 function initLastUpdateDates() {
 	if (location.pathname.startsWith('/umineko/tr-installation')) {
-		replaceLastUpdateDate('umineko-scripting-tr');
+		replaceLastUpdateDate(
+			'umineko-scripting-tr',
+			'UMINEKO_LAST_UPDATE_SCRIPTS',
+		);
 	} else if (location.pathname.startsWith('/higurashi/installation')) {
-		replaceLastUpdateDate('higurashi-scripting-tr');
+		replaceLastUpdateDate(
+			'higurashi-scripting-tr',
+			'HIGURASHI_LAST_UPDATE_SCRIPTS',
+		);
 	}
 }
 
--- mkdocs/docs/umineko/tr-installation.md
+++ mkdocs/docs/umineko/tr-installation.md
@@ -17,10 +17,13 @@ comments: true
 
 Aşağıda bulunan Türkçe dosyaların ikisini de bilgisayarınıza indirin ve ikisini de **Umineko Project**'in kurulu olduğu klasörünüze çıkartın.
 
-**Son Güncelleme:** `<Tarih>`
+**Son Güncelleme:** `<UMINEKO_LAST_UPDATE_SCRIPTS>`  
+**Yayınlanan Son Sürüm:** `<LAST_UPDATE_VERSION>`
 
 [:fontawesome-solid-download: Türkçe Script](https://github.com/Witch-Love/umineko-scripting-tr/releases/latest/download/umineko-tr-scripts.zip){ .md-button .md-button--primary }
 [:fontawesome-solid-download: Türkçe Grafikler](https://github.com/Witch-Love/umineko-scripting-tr-files/releases/latest/download/umineko-tr-files.zip){ .md-button .md-button--primary }
+
+*Sürümünüzün güncel olup olmadığını kontrol etmek için oyunun menü ekranında bulunan sürüm kontrol etme yazısına tıklayabilir veya manuel olarak [bu sayfadan](checkversion.md) kontrol edebilirsiniz.*
 
 ***
 
